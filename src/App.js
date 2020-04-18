dfc28cf0fe59fae2d265149181cf113df5c5cd06
thenatural86 <philip.smalls86@gmail.com>
2020-04-17 20:09:59 -0400

    modal styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [modalState, setModalState] = useState(false)
+
+  const toggleModalState = () => {
+    setModalState(!modalState)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`modalBackground modalShowing-${modalState}`}>
+        <div className="modalInner">
+          <div className="modalImage">
+            <img
+              src="https://images.unsplash.com/photo-1586984478679-9f1ae1573ae3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+              alt="street"
+            />
+          </div>
+          <div className="modalText">
+            <h2>Modal header</h2>
+            <p>
+              Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
+              IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
+              IpsumLorem IpsumLorem IpsumLorem Ipsum Lorem IpsumLorem IpsumLorem
+              IpsumLorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
+              IpsumLorem IpsumLorem Ipsum
+            </p>
+            <form action="">
+              <button>Join Now!</button>
+            </form>
+            <button className="exitButton" onClick={() => toggleModalState()}>
+              exit
+            </button>
+          </div>
+        </div>
+      </div>
+      <button onClick={() => toggleModalState()}>Open Modal</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
